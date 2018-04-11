97ba1aea889290be589c0ca610887e4a8ccb2337
Kylapaallikko <Kylapaallikko@users.noreply.github.com>
2018-04-11 09:33:18 +0300

    Update fail2ban_exporter.py
    Group by jail

--- fail2ban_exporter.py
+++ fail2ban_exporter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from re import compile, findall
-from subprocess import PIPE, run
+from subprocess import PIPE, run, getoutput
 from time import sleep
 from os import getenv
 
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import Gauge, start_http_server, Counter
 from prometheus_client.core import REGISTRY, GaugeMetricFamily
 
 
@@ -15,22 +15,27 @@ cmd = "{path}fail2ban-client status {service}"
 comp = compile(r'\s([a-zA-Z\s]+):\t([a-zA-Z0-9-,\s]+)\n')
 
 
-def extract_data(jail=""):
-    r = run(cmd.format(path=path, service=jail),
-            stdout=PIPE, check=True, shell=True)
-    return findall(comp, ''.join(bytes(r.stdout).decode('utf-8')).lower())
-
-
-def get_jails(jails):
-    return str(jails[1][1]).split(",")
-
-
 class GaugeCollector(object):
+
     def collect(self):
-        for jail in get_jails(extract_data()):
-            for label, value in extract_data(jail):
-                yield GaugeMetricFamily("fail2ban_{}_{}".format(jail.strip(), label.replace(" ", "_")),
-                                        "", float(value))
+        for jail in self.get_jails(self.extract_data()):
+            g = GaugeMetricFamily("fail2ban_{}".format(
+                self.snake_case(jail)), "", labels=['type'])
+            for label, value in self.extract_data(jail):
+                g.add_metric(
+                    [self.snake_case(label)], float(value))
+            yield g
+
+    def get_jails(self, jails):
+        return jails[1][1].split(",")
+
+    def extract_data(self, jail=""):
+        r = run(cmd.format(path=path, service=jail),
+                stdout=PIPE, check=True, shell=True)
+        return findall(comp, ''.join(bytes(r.stdout).decode('utf-8')).lower())
+
+    def snake_case(self, string):
+        return string.strip().replace("-", "_").replace(" ", "_")
 
 
 # Code execution starts from here
